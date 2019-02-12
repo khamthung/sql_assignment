546c58ea897087bfc46e0be05645685f4ba68f3f
james <khamthung@jamess-mbp.home>
2019-02-12 01:17:19 -0800

    update sql

--- sql_assignment.sql
+++ sql_assignment.sql
@@ -90,34 +90,75 @@ INNER JOIN film f ON fa.film_id = f.film_id
 GROUP BY fa.film_id;
 
 -- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
-SELECT
-FROM
+SELECT COUNT(i.film_id) as 'Number of Copies'
+FROM inventory i
+INNER JOIN film f ON i.film_id = f.film_id
+WHERE f.title = 'Hunchback Impossible';
 
 -- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
-
+SELECT last_name, first_name, SUM(p.amount)
+FROM payment p
+INNER JOIN customer c on p.customer_id = c.customer_id
+GROUP BY c.last_name, c.first_name
+ORDER BY c.last_name;
 
 
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
 
+SELECT title
+FROM film
+WHERE (title LIKE 'K%' OR title LIKE 'Q%') AND language_id in 
+															(SELECT language_id
+															FROM language
+															WHERE name ='English');
+
+
 
 -- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
-
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN(SELECT actor_id 
+					FROM film_actor
+					WHERE film_id IN (SELECT film_id 
+										FROM film
+										WHERE title = 'Alone Trip'));
 
 -- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
-
+SELECT customer.first_name, customer.last_name, customer.email, country.country
+FROM customer
+INNER JOIN address ON customer.address_id = address.address_id
+INNER JOIN city ON address.city_id = city.city_id
+INNER JOIN country ON city.country_id = country.country_id
+WHERE country.country ='Canada';
 
 -- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
-
+SELECT f.title as 'Family Films Title'
+FROM film f
+INNER JOIN film_category fc ON f.film_id = fc.film_id
+INNER JOIN category c ON fc.category_id = c.category_id
+WHERE c.name = 'Family';
 
 -- 7e. Display the most frequently rented movies in descending order.
-
+SELECT f.title, COUNT(f.film_id) as 'Number of Rents'
+FROM rental r
+INNER JOIN inventory i ON r.inventory_id = i.inventory_id
+INNER JOIN film f ON i.film_id = f.film_id
+GROUP BY f.film_id
+ORDER BY COUNT(f.film_id) DESC;
 
 -- 7f. Write a query to display how much business, in dollars, each store brought in.
-
+SELECT s.store_id, SUM(p.amount) as 'Total Sales'
+FROM payment p
+INNER JOIN customer c ON p.customer_id = c.customer_id
+INNER JOIN store s ON c.store_id = s.store_id
+GROUP BY s.store_id;
 
 -- 7g. Write a query to display for each store its store ID, city, and country.
-
-
+SELECT store.store_id, city.city, country.country
+FROM store
+INNER JOIN address ON store.address_id = address.address_id
+INNER JOIN city ON address.city_id = city.city_id
+INNER JOIN country ON city.country_id = country.country_id;
 -- 7h. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
 
 
